9cf812d33fd6cb11ec37e17760884cafcc4952cb
Murilo Sanches <murilando@gmail.com>
2020-03-31 11:17:54 -0300

    Resources do Identity

---
+++ AuthenticationServer/EntityFramework/AuthServerDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthenticationServer.EntityFramework
+{
+    public class AuthServerDbContextFactory : IDesignTimeDbContextFactory<AuthServerDbContext>
+    {
+        public AuthServerDbContext CreateDbContext(string[] args)
+        {
+            var configuration = BuildConfiguration();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AuthServerDbContext>()
+                        .UseMySql(configuration.GetConnectionString("Default"));
+
+            return new AuthServerDbContext(optionsBuilder.Options);
+        }
+
+        private IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
+        }
+    }
+}
